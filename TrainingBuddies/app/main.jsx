2fcb5e9587354749d3b001ab75cd15912d475e29
einarsandberg <einars92@gmail.com>
2016-04-21 20:39:00 +0200

    added gulpfile

--- TrainingBuddies/app/main.jsx
+++ TrainingBuddies/app/main.jsx
@@ -1,17 +1,17 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
-var ActivitiesList = require("./components/ActivitiesList.jsx");
-var ActivitiesStore = require("./stores/ActivitiesStore");
-var _activities = [];
+var activityPage = require("./activityPage.jsx");
 
-var getActivitiesCallback = function(activities){
-	_activities = activities;
-	console.log(_activities);
-	render();
-}
-
-ActivitiesStore.onChange(getActivitiesCallback);
-
-function render(){
-    ReactDOM.render(<ActivitiesList activities={_activities} />, document.getElementById("container"));    
-}
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        Hello, world! I am a CommentBox.
+      </div>
+    );
+  }
+});
+ReactDOM.render(
+  <CommentBox />,
+  document.getElementById('content')
+);
