e7771e533c7bcb5243c972e51400754dfac91391
einarsandberg <einars92@gmail.com>
2016-04-25 17:39:32 +0200

    now works locally and on heroku Q

--- TrainingBuddies/server/server.js
+++ TrainingBuddies/server/server.js
@@ -4,17 +4,15 @@ var mongoose = require('mongoose');
 var http = require('http');
 var app = express();
  var path = require("path");
-var uri = process.env.MONGODB_URI;
-console.log(uri);
-//mongoose.connect("mongodb://heroku_f84lvj7p:cc9ono05fvtrkehhvonnfgc1q8@ds047642.mlab.com:47642/heroku_f84lvj7p" || 'mongodb://localhost/trainingbuddies');
-mongoose.connect(uri);
+console.log(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/trainingbuddies');
 app.use(express.static(path.join(__dirname,"../app/dist")));
 app.use(bodyparser.json());
 var activityController = require("./controllers/activityController");
  app.use("/api", activityController);
 var server = http.createServer(app);
 
-var port = process.env.PORT; //|| 3000;
+var port = process.env.PORT || 3000;
 console.log("PORT: " + port);
 console.log("URI" + process.env.MONGOLAB_URI)
 app.listen(port, function() {
