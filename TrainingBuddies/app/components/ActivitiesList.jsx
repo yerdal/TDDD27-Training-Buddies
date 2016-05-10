4d278be0857cd749fb4f6292719c4770256399c6
einarsandberg <einars92@gmail.com>
2016-05-10 15:09:48 +0200

    joining activities now works

--- TrainingBuddies/app/components/ActivitiesList.jsx
+++ TrainingBuddies/app/components/ActivitiesList.jsx
@@ -4,18 +4,9 @@ var AddActivity = require("./AddActivity.jsx");
 
 module.exports = React.createClass({
 
-    getInitialState:function(){
-        var name = this.props.user[1] + " " + this.props.user[2];
-        return({
-            userToken:this.props.user[0],
-            userName:name
-        });
-    },
-
    render:function(){
     /* Have no idea why I cant just set usertoken and username directly as state in render*/
-        var token = this.state.userToken;
-        var name = this.state.userName;
+        var us = this.props.user;
        return(
            <div className="row">
                 <div className="col-md-6">
@@ -28,7 +19,7 @@ module.exports = React.createClass({
 
                         this.props.activities.map(function(s,index){
                             return(
-                                <ActivityInfo info={s} key={"activity"+index} userToken={token} userName={name}  />
+                                <ActivityInfo info={s} key={"activity"+index} user={us}  />
                             )         
                         })
 
