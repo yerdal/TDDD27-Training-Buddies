969aa7f6e4c89626622d6a63c47c5bad12fbfd06
einarsandberg <einars92@gmail.com>
2016-05-25 15:24:45 +0200

    can now click on name under participated activities and see public profile

--- TrainingBuddies/app/components/profilePage.jsx
+++ TrainingBuddies/app/components/profilePage.jsx
@@ -2,7 +2,7 @@ var React = require('react');
 var UserActivities = require('./UserActivities.jsx');
 var ActivitiesStore = require('../stores/activitiesStore');
 var JoinedActivities = require('./JoinedActivities.jsx');
-
+var PublicProfile = require("./PublicProfile.jsx");
 module.exports = React.createClass({
 
 	getInitialState: function(){
@@ -11,7 +11,8 @@ module.exports = React.createClass({
 			user:this.props.user,
 			activities: [], 
 			usrActivities: [],
-			joinedActivites: []
+			joinedActivites: [],
+			showPublicProfile:{show:false, owner:[]}
 			};
 
 	},
@@ -56,6 +57,11 @@ module.exports = React.createClass({
 		});
 
 	},
+	onChildChanged:function(newState){
+		this.setState({
+		  showPublicProfile:newState
+		});
+	},
 
 	render: function(){
 		/* 
@@ -63,24 +69,28 @@ module.exports = React.createClass({
 		4:Profile picture , 5:City , 6:Country, 7:Age
 		*/
 		return(
-
-			<div className="row">
-				<div className="col-md-4">
-					<div id="infoBox">
-					<img className="img-circle img-responsive" src={this.state.user[4]} />
-						<h2> Name: {this.state.user[1]} {this.state.user[2]}</h2>
-						<h4> City: {this.state.user[5]}, {this.state.user[6]} </h4>
-						<h5> Age: {this.state.user[7]}</h5>
+			<div>
+			{!this.state.showPublicProfile.show ?
+				<div className="row">
+					<div className="col-md-4">
+						<div id="infoBox">
+						<img className="img-circle img-responsive" src={this.state.user[4]} />
+							<h2> Name: {this.state.user[1]} {this.state.user[2]}</h2>
+							<h4> City: {this.state.user[5]}, {this.state.user[6]} </h4>
+							<h5> Age: {this.state.user[7]}</h5>
+						</div>
+					</div>
+					<div>
+						<h4>Your activities:</h4>
+						<UserActivities usrActivities={this.state.usrActivities} user={this.state.user}/>
+					</div>
+					<div>
+						<h4>Participated activities: </h4>
+						<JoinedActivities joinedActivities={this.state.joinedActivites} user={this.state.user}
+						initialClicked={this.state.showPublicProfile} callbackParent={this.onChildChanged} />
 					</div>
 				</div>
-				<div>
-					<h4>Your activities:</h4>
-					<UserActivities usrActivities={this.state.usrActivities} user={this.state.user}/>
-				</div>
-				<div>
-					<h4>Participated activities: </h4>
-					<JoinedActivities joinedActivities={this.state.joinedActivites} user={this.state.user} />
-				</div>
+			: <PublicProfile  profile={this.state.showPublicProfile.owner} /> }
 			</div>
 
 
